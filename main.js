944414bc2e5af7a591dfced16e6e2b9c94683c57
IsangikaRathanayaka <104941369+IsangikaRathanayaka@users.noreply.github.com>
2022-05-05 15:05:29 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,14 +2,14 @@ $(document).ready(function(){
      
           var clientId = "588609167741-ohbbiqnddh4ofhfoe854b0k15upfc06l.apps.googleusercontent.com";
 
-         var redirect_uri = "http://localhost:8080/upload/upload.html";
+         var redirect_uri = "http://localhost:8080/upload/send.html";
 
          var scope = "https://www.googleapis.com/auth/drive";
 
-          var url = "http://localhost:8080/upload/upload.html";
+          var url = "http://localhost:8080/upload/send.html";
 
-    //("#login").click(function(){
-     $("#login").click(function(){
+   
+     $("#Auth4").click(function(){
 
              signIn(clientId,redirect_uri,scope,url);
 
@@ -21,9 +21,7 @@ $(document).ready(function(){
         +"&prompt=consent&response_type=code&client_id="+clientId+"&scope="+scope
         +"&access_type=offline";
 
-        // this line makes the user redirected to the url
-
-        window.location = url;
+          window.location = url;
 
      }
 });
